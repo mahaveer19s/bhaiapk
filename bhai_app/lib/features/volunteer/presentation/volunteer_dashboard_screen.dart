f05625ca8af5c8a48bd22414fd611fb60873eaf6
Mahaveer <mahaveer621s@gmail.com>
2026-07-19 11:09:19 +0530

    Fix import name mismatch for latlong package and restore onboarding layout

--- bhai_app/lib/features/volunteer/presentation/volunteer_dashboard_screen.dart
+++ bhai_app/lib/features/volunteer/presentation/volunteer_dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
 import '../../../core/theme/app_theme.dart';
 
